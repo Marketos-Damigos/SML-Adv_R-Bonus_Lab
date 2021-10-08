17f3f59615964c4939d408cf736c3e2135c8210e
Marketos-Damigos <anubis.coder@gmail.com>
2021-10-08 16:10:23 +0000

    linreg package from Lab4

---
+++ ext_print.R
@@ -0,0 +1,12 @@
+#' Print function
+#'
+#' @description Help function to print contents out of the linreg class
+#' @param ... Value to print
+#' @usage ext_print(...)
+#' @return Prints the content that is assigned
+#' @export ext_print
+
+
+ext_print <- function(...) {
+    print(...)
+}
